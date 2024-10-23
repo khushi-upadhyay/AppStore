c882e36bde3359e4c9098d6179b1aaa97d999a74
khushi upadhyay <khushiupadhyay2004@gmail.com>
2024-10-23 12:27:49 +0530

    added section 2

---
+++ AppStore/StandardCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  StandardCollectionViewCell.swift
+//  AppStore
+//
+//  Created by admin70 on 23/10/24.
+//
+
+import UIKit
+
+class StandardCollectionViewCell: UICollectionViewCell {
+    
+}
